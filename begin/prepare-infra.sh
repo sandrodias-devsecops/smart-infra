ad4dc9db7d50657fad7d4e57249b7c0396efbe31
sandrodias-devsecops <sandrodias.oficiall@gmail.com>
2022-03-10 21:17:34 -0300

    Teste inicial do prepare.sh

---
+++ begin/prepare-infra.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Variáveis
+
+
+
+# Trocar a senha do root
+clear
+echo ""
+echo ""
+echo ""
+echo "     * * *                  S M A R T  -  I N F R A                  * * *     "
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "                                                        ...preparando ambiente!"
+echo ""
+echo ""
+echo "     --=> Trocando a senha de root"
+echo ""
+sudo passwd root
+clear
+echo ""
+echo ""
+echo ""
+echo "     * * *                  S M A R T  -  I N F R A                  * * *     "
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "                                                        ...preparando ambiente!"
+echo ""
+echo ""
+echo "     --=> Trocando a senha de root          --OK!--"
+echo ""
