dcd762e820cda21b4a7c60dd02ad1e2f173841eb
sandrodias-devsecops <sandrodias.oficiall@gmail.com>
2022-03-14 16:22:19 -0300

    Criação do disvory com cabeçalho

---
+++ discovery/discovery-infra.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Projeto: Smart-Infra é uma coleção de scripts em Bash para automação de infraestrutura de rede em linux.
+# Hospedado:  atualmente em https://github.com/sandrodias-sysadmin/smart-infra
+# Autor: Sandro Dias
+# E-mail: sandrodias
+# Caminho Absoluto: /smart-infra/discovery/discovery-infra.sh
+# Função: Levantar informações cruciais sobre o sistema, rede e etc guardando-as em variáveis
+# Atualizado em: 14/03/2022
+# Versao: 0.1 
+# 
