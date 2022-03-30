41ca4275007e689003d7358e3d640b389dbff56d
sandrodias-sysadmin <sandrodias.oficiall@gmail.com>
2022-03-29 21:24:11 -0300

    Adicionado o sendEmail nos pre requisitos

--- install-smartinfra.sh
+++ install-smartinfra.sh
@@ -297,7 +297,7 @@ InstallRequirements_CompletedStep=$(echo -e "       Instalação de Pacotes Esse
 	echo -e "$AptDistUpgrade_CompletedStep"
 	echo -e "$InstallRequiriments_CompletedStep"
 	echo -e "\n\n                                  --- = ---\n\n"
-	sudo apt install -y figlet neofetch git wget curl unzip unrar bunzip
+	sudo apt install -y figlet neofetch git wget curl unzip unrar bunzip sendEmail
 	date_installed=$(cat /smart-infra/logs/status/installedstep.log | awk '{ print $1" "$2 }' )
 	phase_installed=$(cat /smart-infra/logs/status/installedstep.log | awk '{ print $3 }' )
 	if [ $? = 0 ]; then
